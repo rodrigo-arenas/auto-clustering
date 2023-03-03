42463d69569a58bb65d11ef42514f65ea96e9b0e
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2023-03-03 09:14:01 -0500

    metric mode optimization

--- autoclustering/autoclustering.py
+++ autoclustering/autoclustering.py
@@ -7,9 +7,10 @@ from .pipelines import get_pipelines
 
 
 class AutoClustering:
-    def __init__(self, alpha: float = 0.5, num_samples=25):
+    def __init__(self, alpha: float = 0.5, num_samples: int = 25, metric: str = 'multimetric'):
         self.alpha = alpha
         self.num_samples = num_samples
+        self.metric = metric
 
     def fit(self, X):
         pipelines = get_pipelines(preprocessing_config, clustering_config)
@@ -19,12 +20,13 @@ class AutoClustering:
             tuner = tune.Tuner(tune.with_parameters(self.train, pipe=pipe["pipe"], X=X),
                                param_space=pipe["params"],
                                tune_config=tune.TuneConfig(
-                                   search_alg=OptunaSearch(metric=["silhouette", "davies_bouldin", "score"],
-                                                           mode=["max", "min", "min"]),
+                                   search_alg=OptunaSearch(),
+                                   metric=self.metric,
+                                   mode=self.optimization_mode_,
                                    num_samples=self.num_samples)
                                )
             trial_result = tuner.fit()
-            best_result = trial_result.get_best_result(metric="score", mode="min")
+            best_result = trial_result.get_best_result(metric=self.metric, mode=self.optimization_mode_)
             results.append(best_result)
 
         return results
@@ -46,6 +48,16 @@ class AutoClustering:
             silhouette_score = -1000
             davies_bouldin = 1000
 
-        score = self.alpha*(1-silhouette_score) + (1-self.alpha)*davies_bouldin
+        multimetric = self.alpha * (1 - silhouette_score) + (1 - self.alpha) * davies_bouldin
 
-        return {"silhouette": silhouette_score, "davies_bouldin": davies_bouldin, "score": score}
+        return {"silhouette": silhouette_score, "davies_bouldin": davies_bouldin, "multimetric": multimetric}
+
+    @property
+    def optimization_mode_(self):
+        modes_map = {"multimetric": 'min', "davies_bouldin": 'min', "silhouette": 'max'}
+        metric_mode = modes_map.get(self.metric)
+        if not metric_mode:
+            raise ValueError(f"Metric must be one of ['multimetric', 'davies_bouldin', 'silhouette'], "
+                             f"but got {self.metric} instead")
+
+        return metric_mode
