a2148a8282e8cd11dd8365d90f45fdf35649e39b
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2023-03-03 22:50:47 -0500

    add preprocessing steps

--- autoclustering/configs/preprocessing.py
+++ autoclustering/configs/preprocessing.py
@@ -1,6 +1,9 @@
 from dirty_cat import TableVectorizer
+from sklearn.preprocessing import RobustScaler, MinMaxScaler
 
 preprocessing_config = [
+    {"model": 'passthrough', "params": {}},
     {"model": TableVectorizer(), "params": {}},
-    {"model": 'passthrough', "params": {}}
+    {"model": RobustScaler(), "params": {}},
+    {"model": MinMaxScaler(), "params": {}}
 ]
