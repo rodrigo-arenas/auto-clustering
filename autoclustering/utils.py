c56619623c9d5909724e3905fc99024ba531df75
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2023-03-04 20:22:15 -0500

    penalization as a parameter

--- autoclustering/utils.py
+++ autoclustering/utils.py
@@ -1,10 +1,10 @@
-import numpy as np
+from pandas.errors import InvalidIndexError
 
 from sklearn import metrics
 from hdbscan.validity import validity_index
 
 
-def score_candidate(pipe, X):
+def score_candidate(pipe, X, penalization=1000):
     """
     Scores the 'quality' of a clustering pipeline
 
@@ -14,26 +14,28 @@ def score_candidate(pipe, X):
         pipeline to evaluate the results
     X : array-like of shape (n_samples, n_features)
             The data to fit. Can be for example a list, or an array.
+    penalization : int, default=1000
+        penalization value to the score when not clusters found
 
     Returns
     -------
     score: dict
         Dict with popular clustering metrics
     """
-    labels = pipe.named_steps["clustering"].labels_
-    n_labels = len(np.unique(labels))
-    if n_labels > 1:
+
+    try:
+        labels = pipe.named_steps["clustering"].labels_
         validity_score = validity_index(X, labels)
         silhouette_score = metrics.silhouette_score(X, labels, metric="euclidean")
         davies_bouldin = metrics.davies_bouldin_score(X, labels)
         calinski_harabasz = metrics.calinski_harabasz_score(X, labels)
 
-    else:
-        # Penalize algorithms with one cluster
-        validity_score = -1000
-        silhouette_score = -1000
-        davies_bouldin = 1000
-        calinski_harabasz = -1000
+    except (InvalidIndexError, ValueError, AttributeError):
+        # Penalize algorithms with one cluster or with distance errors
+        validity_score = -penalization
+        silhouette_score = -penalization
+        davies_bouldin = penalization
+        calinski_harabasz = -penalization
 
     return {"silhouette": silhouette_score,
             "davies_bouldin": davies_bouldin,
