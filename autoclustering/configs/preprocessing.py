fa657d71ed5dc812e6a7d2ee692fbe24a2b0ea4d
rodrigo.arenas <31422766+rodrigo-arenas@users.noreply.github.com>
2023-03-04 20:21:58 -0500

    params on RobustScaler

--- autoclustering/configs/preprocessing.py
+++ autoclustering/configs/preprocessing.py
@@ -1,9 +1,11 @@
 from dirty_cat import TableVectorizer
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
+from ray import tune
 
 preprocessing_config = [
     {"model": 'passthrough', "params": {}},
     {"model": TableVectorizer(), "params": {}},
-    {"model": RobustScaler(), "params": {}},
+    {"model": RobustScaler(), "params": {"with_centering": tune.choice([True, False]),
+                                         "unit_variance": tune.choice([True, False])}},
     {"model": MinMaxScaler(), "params": {}}
 ]
